da45920cab16d097f1123ab09b36c7a9ae38e562
faizzaidi23 <786faizzaidi@gmail.com>
2026-01-22 19:13:59 +0530

    chip basics

---
+++ app/src/main/java/com/example/FullComposeOfficialDocumentation/Components_3/Chip_10/1_chip.kt
@@ -0,0 +1,25 @@
+package com.example.FullComposeOfficialDocumentation.Components_3.Chip_10
+
+/*
+Chip component is a compact interactive UI element.
+It represents complex entities like a contact or a tag, often with an icon and label. It can be checkable, dismissible or clickable
+*/
+
+/*
+Four types of chips
+
+
+Assist-->Guides the user during a task. Often appears as a temporary UI element in response to user input
+Filter-->Allows the users to refine content from a set of options. They can be selected or deselected and may include a checkmark incon when selected
+Input--->Represents user provided information such as selections in a menu They can contain an icon and text and provide X for removal
+Suggestion--->Provides recommendation to the user based on their recent activity or input.
+Typically appear beneath an input field to prompt the user actions.
+*/
+
+
+/*
+API surface--->
+label--->The string that appears on the chip
+icon-->The icon displayed at the start of the chip. Some of the specific composable have a separate leadingIcon and trailingIcon parameter
+onClick-->The lambda that the chip calls when the user presses it
+*/
