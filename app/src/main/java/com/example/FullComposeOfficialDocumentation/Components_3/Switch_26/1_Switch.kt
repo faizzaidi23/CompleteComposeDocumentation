5e5b9fb56602fc9454b96e8b2a52d4fbfb090267
faizzaidi23 <786faizzaidi@gmail.com>
2026-01-23 21:52:34 +0530

    done with the basic switch implementation

--- app/src/main/java/com/example/FullComposeOfficialDocumentation/Components_3/Switch_26/1_Switch.kt
+++ app/src/main/java/com/example/FullComposeOfficialDocumentation/Components_3/Switch_26/1_Switch.kt
@@ -22,6 +22,26 @@ enabled-->Whether the switch is enabled or disabled
 colors-->The colors used for the switch
 */
 
+/*
+what is it in kotlin-->
+
+If a labda has only one parameter we do not need to name it kotlin automatically calls it it
+
+
+so instead of writeitn
+onCheckedChange={value->
+checked=value
+
+
+we can simply write
+onCheckedChange={
+checked=it
+}
+
+"Hey Compose,
+when YOU call this function and give me a Boolean, I will receive it as value and update my state."
+*/
+
 
 @Composable
 fun SwitchExample(){
