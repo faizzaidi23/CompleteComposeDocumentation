c8ca2199314ce60154791227936f2c5f85aa886f
faizzaidi23 <786faizzaidi@gmail.com>
2026-02-08 19:35:41 +0530

    accessibility in compose

---
+++ app/src/main/java/com/example/FullComposeOfficialDocumentation/Accessibility_9/1_OverView.kt
@@ -0,0 +1,31 @@
+package com.example.FullComposeOfficialDocumentation.Accessibility_9
+
+/*
+Developing with accessibility in mind means making your apps usable for everyone, including people with accessibility needs, who may use Android devices in many different ways.
+Compose provides a foundation for building more accessible UIs with its declarative APIs and tools that help make your apps more inclusive.
+*/
+
+/*
+There are several key and supporting concepts in compose accessibility:
+
+
+API Defaults --> Learning how Compose handles accessibility by default and how to leverage semantics and patterns to support accessibility from the start and use them for
+custom components.
+
+Semantics --> Understand the system of representing the meaning and role of UI elements for accessibility services and how to choose appropriate semantics to represent properties like
+content types, descriptions, and states.
+
+Modify Traversal Order -->
+Modify the order in which accessibility services navigate through elements on screen, which can be customized for better user experience.
+
+Support User-Scalable Content -->
+Allow users to adjust the size of text and UI elements in your app to fit their needs.
+
+Merging and Clearing -->
+Understand semantic merging and clearing strategies and APIs, and when it is appropriate to hide semantics from accessibility services.
+
+Inspect and Debug --> Inspect your composables'
+ accessibility semantics with tools and debug unexpected behaviors when using Android's assistive technologies.
+
+Testing --> Detect common accessibility issues and automate some aspects of testing with Compose accessibility checks.
+*/
