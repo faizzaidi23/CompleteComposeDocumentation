371ce93f1ebeceea2076716d13eb6fc050944a55
faizzaidi23 <786faizzaidi@gmail.com>
2026-02-12 23:41:04 +0530

    just did

---
+++ app/src/main/java/com/example/FullComposeOfficialDocumentation/Accessibility_9/Support_user_scalable_content/1_basic.kt
@@ -0,0 +1,9 @@
+package com.example.FullComposeOfficialDocumentation.Accessibility_9.Support_user_scalable_content
+
+/*
+implementing the pinch to zoom gestures to support scalable content in the app
+this is the standard platform consistent method for improving accessibility,
+allowing the users to intuitively adjust the size of the text and the ui elements to fit their needs
+the app can define custom scaling behavior with granular control and contextual behavior that offers an experience that users
+often discover more quickly than a system level feature like screen magnification
+*/
