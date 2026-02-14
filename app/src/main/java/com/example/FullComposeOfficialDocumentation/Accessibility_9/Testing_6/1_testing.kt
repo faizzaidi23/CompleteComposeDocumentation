a61ce808cea19d2d653b4f7f5a50b7af9a16478a
faizzaidi23 <786faizzaidi@gmail.com>
2026-02-14 09:50:53 +0530

    overview of thtouch and input in jetpack

---
+++ app/src/main/java/com/example/FullComposeOfficialDocumentation/Accessibility_9/Testing_6/1_testing.kt
@@ -0,0 +1,9 @@
+package com.example.FullComposeOfficialDocumentation.Accessibility_9.Testing_6
+
+/*
+An essential way of testing accessibility is a form of manual testing
+by turning accessibility services, like TalkBack or Switch Access on and checking
+if everything works as expected.
+this provides direct insight into how uses with accessibility needs might experience your application
+*/
+
---
+++ app/src/main/java/com/example/FullComposeOfficialDocumentation/TouchAndInput_10/overView.kt
@@ -0,0 +1,12 @@
+package com.example.FullComposeOfficialDocumentation.TouchAndInput_10
+
+/*
+users interact with android apps in various different ways like they can
+
+Tap on a button with their finger
+Navigate through a screen using their phhhysical keyboard
+enter their email address using the on screen keyboard
+
+compose has a lot of built in support for these use cases but in some scenarios we need to
+customize or extent the default behavior.
+*/
